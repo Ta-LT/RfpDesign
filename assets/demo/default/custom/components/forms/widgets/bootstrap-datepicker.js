c70829955ba0e2f9c3e1a6c9d8949dfd9281412e
ltinter <5201249@qq.com>
2018-08-20 01:30:43 +0800

    添加宝马项目计划书相关文件

--- assets/demo/default/custom/components/forms/widgets/bootstrap-datepicker.js
+++ assets/demo/default/custom/components/forms/widgets/bootstrap-datepicker.js
@@ -33,6 +33,14 @@ var BootstrapDatepicker = function () {
                 rightArrow: '<i class="la la-angle-right"></i>'
             }
         });
+        $('.input-group.date').datepicker({
+            todayHighlight: true,
+            orientation: "bottom left",
+            templates: {
+                leftArrow: '<i class="la la-angle-left"></i>',
+                rightArrow: '<i class="la la-angle-right"></i>'
+            }
+        });
 
         // input group layout for modal demo
         $('#m_datepicker_2_modal').datepicker({
